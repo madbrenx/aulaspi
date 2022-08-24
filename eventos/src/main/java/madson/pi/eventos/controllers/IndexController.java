5b340492e25a6395475eb4559d0effc93e7ce8d2
Madson Breno Costa Nascimento <madson.breno@academico.ifrn.edu.br>
2022-08-24 17:43:05 -0300

    colocando os arquivos iniciais na minha aplicação

---
+++ eventos/src/main/java/madson/pi/eventos/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package madson.pi.eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+		@RequestMapping("/")
+		public String index() {
+			System.out.println("chamou o método index");
+			return "home";	
+		}
+		
+		
+}
