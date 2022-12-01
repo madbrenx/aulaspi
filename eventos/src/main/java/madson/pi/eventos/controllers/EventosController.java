eb7be96e51eeec296341e4bd06400d539125a98c
Madson Breno Costa Nascimento <madson.breno@academico.ifrn.edu.br>
2022-12-01 17:30:30 -0300

    Atualizando layout da aplicação

--- eventos/src/main/java/madson/pi/eventos/controllers/EventosController.java
+++ eventos/src/main/java/madson/pi/eventos/controllers/EventosController.java
@@ -15,14 +15,14 @@ public class EventosController {
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
-		return "FormEvento";
+		return "eventos/FormEvento";
 	}
 
 	@RequestMapping(path = "/eventos", method = RequestMethod.POST)
 	public String adicionar(Evento evento) {
 		System.out.println(evento);
 		er.save(evento);
-		return "evento-adicionado";
+		return "eventos/evento-adicionado";
 	}
 
 }
